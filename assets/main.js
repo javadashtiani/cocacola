6e8fb7a88abc315fb8b5411c622fae23516f6ad1
Javad <134012615+javadashtiani@users.noreply.github.com>
2023-05-29 01:48:22 +0330

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,6 @@
+let hamburgerIcon = document.getElementById("hamburgerMenu");
+let hemburgerMenu = document.getElementById("showMenu");
+
+hamburgerIcon.addEventListener("click", function () {
+  hemburgerMenu.classList.toggle('show');
+});
